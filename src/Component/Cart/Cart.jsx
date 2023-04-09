3ae30b981df8823b71d862b1291bab45ec4ec2eb
Mahedi Hasan <writetomahedi@gmail.com>
2023-04-10 03:21:52 +0600

    save cart to localstorage

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -4,14 +4,24 @@ import './Cart.css'
 const Cart = (props) => {
     // const cart = props.cart; option: 1
     const {cart} =props 
+   
+    let total = 0;
+    let totalShipping  = 0;
+    for(const product of cart ){
+         total = total+ product.price
+         totalShipping = totalShipping + product.shipping
+    }
+    const tax = total*7/100
+    const grandTotal = total+totalShipping+tax
+ 
     return (
         <div className='cart'>
              <h3>Order Summary</h3>
                 <h4>Selected Items:{cart.length}</h4>
-                <p>Total Price:</p>
-                <p>Total Shipping</p>
-                <p>Tax</p>
-                <h4>Grand Total</h4>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping : ${totalShipping}</p>
+                <p>Tax :$ {tax.toFixed(2)}</p>
+                <h4>Grand Total:$ {grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
